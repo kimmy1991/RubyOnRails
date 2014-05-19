065fd98a60b382aad787e3c23ce0ae43a0eadd09
kimmy <waikin824@googlemail.com>
2014-05-19 20:21:26 +0100

    create account finish

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,25 @@
 class UsersController < ApplicationController
   def new
+  	@user = User.new
+  	#print "abccccc!!!!!!!!"
   end
 
   def show
   	@user = User.find(params[:id])
+  end
+
+  def create
+  	
+  	@user = User.new(params.require(:user).permit(:name, :email, :password, :password_confirmation))
+  	
+  	if @user.save
+  		flash[:success] = "Welcome to the Sample App!"
+  		redirect_to @user
+
+  	else
+  		render 'new'
+  	end
+
   end
   
 end
